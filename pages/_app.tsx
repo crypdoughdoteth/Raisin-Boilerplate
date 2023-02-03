014c13ad882bc4d55a662ae90e4132647cc1ffa7
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-02-02 20:08:18 -0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,45 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, goerli } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [goerli],
+  [
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider coolMode theme={darkTheme({      
+      accentColor: '#7b3fe4',
+      accentColorForeground: 'white',
+      borderRadius: 'small',
+      fontStack: 'system',
+      overlayBlur: 'small',
+      })}chains={chains}>
+  <Component {...pageProps} />
+  </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
